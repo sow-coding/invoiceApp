b06a12f0c1f010ba3519f7fc189b844c3beae71f
Sow-coding <sow.coding@gmail.com>
2023-09-16 17:57:34 +0200

    Final app v2

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,10 @@ import { RecoilRoot } from 'recoil'
 import '../styles/globals.css'
 import { leagueSpartan } from '@/fonts/font'
 
-export const metadata = {
+/*export const metadata = {
   title: 'Invoice app',
   description: 'Nothing',
-}
+}*/
 
 export default function RootLayout({ children }) {
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
 "use client"
-import HomePage from "@/pages/homePage";
+//import HomePage from "@/pages/homePage";
+import SideBar from "@/components/sideBar/sideBar";
+import HomeContent from '@/components/homeContent/homeContent';
+import { showInvoiceFormState } from '@/app/showInvoiceFormState';
+import InvoiceForm from '@/components/invoiceForm/invoiceForm';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+import { deleteInvoice } from '@/app/deleteInvoice';
 
 export default function Home() {
+  const showInvoiceForm = useRecoilValue(showInvoiceFormState)
+
   return (
-      <HomePage />
+      //<HomePage />  
+              <div className="homePage">
+                  {showInvoiceForm ? "":<SideBar />}
+                  {showInvoiceForm && <InvoiceForm/>}  
+                      <HomeContent />
+              </div> 
   )
 }
